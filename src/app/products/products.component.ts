60a3990b6c65ee2d4a6afb1b73e0836db9da3d24
Jemali Ayoub <jemaliayoub1@gmail.com>
2022-09-30 18:31:45 +0100

    initial commit

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../services/product.service";
+import {Product} from "../model/product.model";
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  products !: Array<Product>;
+  errorMessage! : string;
+  constructor( private productService : ProductService) { }
+
+  ngOnInit(): void {
+  this.handleGetAllProducts()
+  }
+  handleGetAllProducts(){
+    this.productService.getAllProducts().subscribe({
+      next:(data)=>{
+        this.products=data;
+      },
+      error:err => {
+        this.errorMessage=err;
+      }
+    });
+  }
+  handDeleteProduct(p: Product) {
+    let conf=confirm("Are you sure");
+    if(!conf) return;
+  this.productService.deleteProduct(p.id).subscribe({
+    next:(data)=>{
+    //this.handleGetAllProducts()
+      let index= this.products.indexOf(p);
+      this.products.splice(index,1);
+    }
+  })
+  }
+}
